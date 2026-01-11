bdd54036b0e95380cfddadee4d4bd2b527c1c624
Priyanka-Kewat <priyankakewat43@gmail.com>
2026-01-11 23:49:02 +0530

    place order

--- tests/Shop.spec.js
+++ tests/Shop.spec.js
@@ -32,6 +32,53 @@ test("Automate shop website", async ({ browser }) => {
   const bools = await page.locator("h3:has-text('iphone 13 pro')").isVisible;
   expect(bools).toBeTruthy();
 
+  await page.locator("text=Checkout").click();
+  await page
+    .locator("[placeholder*='Country']")
+    .pressSequentially("ind", { delay: 150 });
+
+  const dropdown = await page.locator(".ta-results");
+  await dropdown.waitFor();
+  const optCounts = await dropdown.locator("button").count();
+  console.log(optCounts);
+
+  for (let i = 0; i < optCounts; ++i) {
+    const text = await dropdown.locator("button").nth(i).textContent();
+    // await text.waitFor();
+
+    if (text === " India") {
+      await dropdown.locator("button").nth(i).click();
+      break;
+    }
+  }
+
+  expect(page.locator(".user__name [type='text']").first()).toHaveText(
+    "priyankakewat43@gmail.com"
+  );
+
+  await page.locator(".action__submit").click();
+  await expect(page.locator(".hero-primary").first()).toHaveText(
+    " Thankyou for the order. "
+  );
+  const order_id = await page
+    .locator(".em-spacer-1 .ng-star-inserted")
+    .textContent();
+  console.log(order_id);
+
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator("tbody").waitFor();
+  const rows = await page.locator("tbody tr");
+
+  for (let i = 0; i < (await rows.count()); ++i) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (order_id.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(order_id.includes(orderIdDetails)).toBeTruthy();
+
   //   await page.locator(".text-reset").click();
   //   await page.locator("input#firstName").fill("Priyanka");
   //   await page.locator("[type=lastName]").fill("Kewat");
